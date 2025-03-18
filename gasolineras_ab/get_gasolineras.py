ee1ec0b1973c94fb00bd52b61313e0f1cb8df394
nikodyulger <nikolasvdulgerov@gmail.com>
2025-03-18 11:34:30 +0100

    obtencion gasolineras albacete

--- gasolineras_ab/get_gasolineras.py
+++ gasolineras_ab/get_gasolineras.py
@@ -1,8 +1,32 @@
 import json
+import time
+import pandas as pd
 import requests as req
+from dataclasses import dataclass, asdict
+from rich.pretty import pprint
 
 
 # Doc de la API https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/help
+@dataclass
+class Gasolinera:
+    id_estacion: str
+    direccion: str
+    cod_postal: str
+    latitud: str
+    longitud: str
+    rotulo: str
+    horario: str
+    municipio: str = "Albacete"
+
+
+@dataclass
+class PrecioGasolinera:
+    id_estacion: str
+    fecha: str
+    precio_gasoleo_a: float = None
+    precio_gasoleo_premium: float = None
+    precio_gasolina_95: float = None
+    precio_gasolina_98: float = None
 
 
 def get_comunidades_autonomas():
@@ -41,6 +65,58 @@ def get_municipios():
     return data
 
 
+def get_precios_fecha_municipio(fecha: str, id_municipio: str):
+    """
+    fecha -> formato dd-mm-aaaa
+    id_municipio -> uno valido de la lista de municipios
+    """
+
+    URL = f"https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestresHist/FiltroMunicipio/{fecha}/{id_municipio}"
+
+    res = req.get(URL)
+    data = res.json()
+    return data
+
+
+def get_gasolineras_ab(data: dict):
+    """
+    data: dict -> json respuesta API
+    {
+        "Fecha": "dd/mm/yyyy 0:00:00",
+        "ListaEESSPrecio": [
+            {
+                "C.P.": "",
+                "Dirección": "",
+                "Horario": "",
+                "Latitud": "",
+                "Localidad": "",
+                "Longitud (WGS84)": ""
+                ...
+            },
+            ...
+        ]
+    }
+    """
+    gasolineras = []
+    precios_estaciones = data["ListaEESSPrecio"]
+    for estacion in precios_estaciones:
+        gasolinera = Gasolinera(
+            id_estacion=estacion["IDEESS"],
+            direccion=estacion["Dirección"],
+            cod_postal=estacion["C.P."],
+            latitud=estacion["Latitud"],
+            longitud=estacion["Longitud (WGS84)"],
+            rotulo=estacion["Rótulo"],
+            horario=estacion["Horario"],
+        )
+        gasolineras.append(gasolinera)
+
+    df = pd.DataFrame(gasolineras)
+    df.to_csv("data/gasolineras_ab.csv", index=False)
+
+    return gasolineras
+
+
 """
     {
         "IDMunicipio": "54",
@@ -55,4 +131,6 @@ def get_municipios():
 if __name__ == "__main__":
     # get_comunidades_autonomas()
     # get_provincias()
-    get_municipios()
+    # get_municipios()
+    data = get_precios_fecha_municipio("15-02-2025", "54")
+    get_gasolineras_ab(data)  # ejecutar solo una vez
