fe900368e4fb64cbd2444d5095c62260d91d4540
nikodyulger <nikolasvdulgerov@gmail.com>
2025-03-18 11:51:00 +0100

    precios gasolineras solo un dia, long y lat pasado a float

--- gasolineras_ab/get_gasolineras.py
+++ gasolineras_ab/get_gasolineras.py
@@ -12,8 +12,8 @@ class Gasolinera:
     id_estacion: str
     direccion: str
     cod_postal: str
-    latitud: str
-    longitud: str
+    latitud: float
+    longitud: float
     rotulo: str
     horario: str
     municipio: str = "Albacete"
@@ -101,13 +101,13 @@ def get_gasolineras_ab(data: dict):
     precios_estaciones = data["ListaEESSPrecio"]
     for estacion in precios_estaciones:
         gasolinera = Gasolinera(
-            id_estacion=estacion["IDEESS"],
-            direccion=estacion["Dirección"],
-            cod_postal=estacion["C.P."],
-            latitud=estacion["Latitud"],
-            longitud=estacion["Longitud (WGS84)"],
-            rotulo=estacion["Rótulo"],
-            horario=estacion["Horario"],
+            id_estacion=estacion.get("IDEESS", ""),
+            direccion=estacion.get("Dirección", ""),
+            cod_postal=estacion.get("C.P.", ""),
+            latitud=float(estacion.get("Latitud").replace(",", ".")),
+            longitud=float(estacion.get("Longitud (WGS84)").replace(",", ".")),
+            rotulo=estacion.get("Rótulo", ""),
+            horario=estacion.get("Horario", ""),
         )
         gasolineras.append(gasolinera)
 
@@ -115,6 +115,63 @@ def get_gasolineras_ab(data: dict):
     df.to_csv("data/gasolineras_ab.csv", index=False)
 
     return gasolineras
+
+
+def get_precios_gasolineras(data: dict):
+    """
+    data: dict -> json respuesta API
+    {
+        "Fecha": "dd/mm/yyyy 0:00:00",
+        "ListaEESSPrecio": [
+            {
+                ...
+                "IDEESS": "",
+                "Precio Gasoleo A": "1,419",
+                "Precio Gasoleo B": "",
+                "Precio Gasoleo Premium": "1,459",
+                "Precio Gasolina 95 E10": "",
+                "Precio Gasolina 95 E5": "1,459",
+                "Precio Gasolina 95 E5 Premium": "",
+                "Precio Gasolina 98 E10": "",
+                "Precio Gasolina 98 E5": "1,659"
+                ...
+            },
+            ...
+        ]
+    }
+    """
+    precios = []
+    fecha = data.get("Fecha").split(" ")[0]
+    precios_estaciones = data.get("ListaEESSPrecio")
+
+    for estacion in precios_estaciones:
+        precio_gasoleo_a = estacion.get("Precio Gasoleo A", "").replace(",", ".")
+        precio_gasoleo_premium = estacion.get("Precio Gasoleo Premium", "").replace(
+            ",", "."
+        )
+        precio_gasolina_95 = estacion.get("Precio Gasolina 95 E5", "").replace(",", ".")
+        precio_gasolina_98 = estacion.get("Precio Gasolina 98 E5", "").replace(",", ".")
+
+        precio = PrecioGasolinera(
+            id_estacion=estacion.get("IDEESS"),
+            fecha=fecha,
+            precio_gasoleo_a=float(precio_gasoleo_a) if precio_gasoleo_a else None,
+            precio_gasoleo_premium=(
+                float(precio_gasoleo_a) if precio_gasoleo_premium else None
+            ),
+            precio_gasolina_95=(
+                float(precio_gasolina_95) if precio_gasolina_95 else None
+            ),
+            precio_gasolina_98=(
+                float(precio_gasolina_98) if precio_gasolina_98 else None
+            ),
+        )
+        precios.append(precio)
+
+    df = pd.DataFrame(precios)
+    df.to_csv("data/precios_gasolineras.csv", index=False)
+
+    return precios
 
 
 """
@@ -133,4 +190,5 @@ if __name__ == "__main__":
     # get_provincias()
     # get_municipios()
     data = get_precios_fecha_municipio("15-02-2025", "54")
-    get_gasolineras_ab(data)  # ejecutar solo una vez
+    # get_gasolineras_ab(data)  # ejecutar solo una vez
+    get_precios_gasolineras(data)  # ejecutar por cada día del año desde 2022 hasta ayer
